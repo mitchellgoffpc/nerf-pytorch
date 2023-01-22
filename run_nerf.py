4b1fa86e1974dcde848646acfc8b5c8cb80fd100
Mitchell Goff <mitchellgoffpc@gmail.com>
2023-01-22 14:56:35 -0800

    Combined run_nerf/run_nerf_helpers

--- run_nerf.py
+++ run_nerf.py
@@ -1,19 +1,129 @@
 import os, sys
 import numpy as np
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
 import configargparse
 from tqdm import tqdm, trange
 from functools import partial
-
-from run_nerf_helpers import *
 from load_llff import load_llff_data
 from load_blender import load_blender_data
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 np.random.seed(0)
 
+# Misc
+img2mse = lambda x, y: torch.mean((x - y) ** 2)
+mse2psnr = lambda x: -10. * torch.log(x) / torch.log(torch.Tensor([10.]))
+to8b = lambda x: (255*np.clip(x,0,1)).astype(np.uint8)
 
+# Positional encoding (section 5.1)
+class Embedder:
+    def __init__(self, num_freqs):
+        self.out_dim = 3
+        self.embed_fns = [lambda x: x]
+
+        freq_bands = 2. ** torch.linspace(0., num_freqs-1, steps=num_freqs)
+        for freq in freq_bands:
+            for p_fn in [torch.sin, torch.cos]:
+                self.embed_fns.append(lambda x, p_fn=p_fn, freq=freq: p_fn(x * freq))
+                self.out_dim += 3
+
+    def __call__(self, inputs):
+        return torch.cat([fn(inputs) for fn in self.embed_fns], -1)
+
+# Model
+class NeRF(nn.Module):
+    def __init__(self, D=8, W=256, input_ch=3, input_ch_views=3):
+        super().__init__()
+        self.D = D
+        self.W = W
+        self.input_ch = input_ch
+        self.input_ch_views = input_ch_views
+        self.skips = [4]
+
+        self.pts_linears = nn.ModuleList([nn.Linear(input_ch, W)] + [nn.Linear(W + (input_ch if i in self.skips else 0), W) for i in range(D-1)])
+        self.views_linears = nn.ModuleList([nn.Linear(input_ch_views + W, W//2)])
+        self.feature_linear = nn.Linear(W, W)
+        self.alpha_linear = nn.Linear(W, 1)
+        self.rgb_linear = nn.Linear(W//2, 3)
+
+    def forward(self, x):
+        input_pts, input_views = torch.split(x, [self.input_ch, self.input_ch_views], dim=-1)
+        h = input_pts
+        for i, l in enumerate(self.pts_linears):
+            h = F.relu(self.pts_linears[i](h))
+            if i in self.skips:
+                h = torch.cat([input_pts, h], -1)
+
+        alpha = self.alpha_linear(h)
+        feature = self.feature_linear(h)
+        h = torch.cat([feature, input_views], -1)
+
+        for i, l in enumerate(self.views_linears):
+            h = F.relu(self.views_linears[i](h))
+
+        rgb = self.rgb_linear(h)
+        return torch.cat([rgb, alpha], -1)
+
+
+# Ray helpers
+def get_rays(H, W, K, c2w):
+    i, j = torch.meshgrid(torch.linspace(0, W-1, W), torch.linspace(0, H-1, H), indexing='xy')
+    dirs = torch.stack([(i-K[0,2]) / K[0,0], -(j-K[1,2]) / K[1,1], -torch.ones_like(i)], -1)
+    # Rotate ray directions from camera frame to the world frame
+    rays_d = torch.sum(dirs[..., np.newaxis, :] * c2w[:3,:3], -1)  # dot product, equals to: [c2w.dot(dir) for dir in dirs]
+    # Translate camera frame's origin to the world frame. It is the origin of all rays.
+    rays_o = c2w[:3,-1].expand(rays_d.shape)
+    return torch.stack([rays_o, rays_d], 0)
+
+def ndc_rays(H, W, focal, near, rays_o, rays_d):
+    # Shift ray origins to near plane
+    t = -(near + rays_o[...,2]) / rays_d[...,2]
+    rays_o = rays_o + t[...,None] * rays_d
+
+    # Projection
+    o0 = -1./(W/(2.*focal)) * rays_o[...,0] / rays_o[...,2]
+    o1 = -1./(H/(2.*focal)) * rays_o[...,1] / rays_o[...,2]
+    o2 = 1. + 2. * near / rays_o[...,2]
+
+    d0 = -1./(W/(2.*focal)) * (rays_d[...,0]/rays_d[...,2] - rays_o[...,0]/rays_o[...,2])
+    d1 = -1./(H/(2.*focal)) * (rays_d[...,1]/rays_d[...,2] - rays_o[...,1]/rays_o[...,2])
+    d2 = -2. * near / rays_o[...,2]
+
+    return torch.stack([o0,o1,o2], -1), torch.stack([d0,d1,d2], -1)
+
+# Hierarchical sampling (section 5.2)
+def sample_pdf(bins, weights, N_samples, det=False):
+    # Get pdf
+    weights = weights + 1e-5 # prevent nans
+    pdf = weights / torch.sum(weights, -1, keepdim=True)
+    cdf = torch.cumsum(pdf, -1)
+    cdf = torch.cat([torch.zeros_like(cdf[...,:1]), cdf], -1)  # (batch, len(bins))
+
+    # Take uniform samples
+    if det:
+        u = torch.linspace(0., 1., steps=N_samples)
+        u = u.expand(list(cdf.shape[:-1]) + [N_samples]).contiguous()
+    else:
+        u = torch.rand(list(cdf.shape[:-1]) + [N_samples])
+
+    # Invert CDF
+    inds = torch.searchsorted(cdf, u, right=True)
+    below = torch.max(torch.zeros_like(inds-1), inds-1)
+    above = torch.min((cdf.shape[-1]-1) * torch.ones_like(inds), inds)
+    inds_g = torch.stack([below, above], -1)  # (batch, N_samples, 2)
+
+    matched_shape = [inds_g.shape[0], inds_g.shape[1], cdf.shape[-1]]
+    cdf_g = torch.gather(cdf.unsqueeze(1).expand(matched_shape), 2, inds_g)
+    bins_g = torch.gather(bins.unsqueeze(1).expand(matched_shape), 2, inds_g)
+
+    denom = (cdf_g[...,1]-cdf_g[...,0])
+    denom = torch.where(denom<1e-5, torch.ones_like(denom), denom)
+    t = (u-cdf_g[...,0])/denom
+    return bins_g[...,0] + t * (bins_g[...,1]-bins_g[...,0])
+
+# Embed inputs and run network
 def run_network(inputs, viewdirs, fn, embed_fn, embeddirs_fn, netchunk=1024*64):
     """Prepares inputs and applies network 'fn'."""
     input_dirs = viewdirs[:,None].expand(inputs.shape)
@@ -21,7 +131,7 @@ def run_network(inputs, viewdirs, fn, embed_fn, embeddirs_fn, netchunk=1024*64):
     outputs_flat = torch.cat([fn(embedded[i:i+netchunk]) for i in range(0, embedded.shape[0], netchunk)], 0)
     return outputs_flat.view(*inputs.shape[:-1], outputs_flat.shape[-1])
 
-
+# Convert network outputs to rgb
 def raw2outputs(raw, z_vals, rays_d, raw_noise_std=0, white_bkgd=False):
     """Transforms model's predictions to semantically meaningful values.
     Args:
@@ -98,7 +208,6 @@ def render_rays(ray_batch, network_fn, network_query_fn, N_samples, perturb=0.,
     rgb_map, weights = raw2outputs(raw, z_vals, rays_d, raw_noise_std, white_bkgd)
 
     return rgb_map, rgb_map_0
-
 
 def render(H, W, K, chunk=1024*32, rays=None, c2w=None, ndc=True, near=0., far=1., **kwargs):
     """Render rays
@@ -191,7 +300,6 @@ def config_parser(argv=sys.argv):
 
 
 def train(args):
-
     # Load data
     if args.dataset_type == 'llff':
         images, poses, bds, render_poses, _ = load_llff_data(args.datadir, args.factor, recenter=True, bd_factor=.75, spherify=args.spherify)
